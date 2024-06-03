d229244db7ac3f4c3265c8bbe8e13dfa159b020c
Erick Ramos <erickramossilva@gmail.com>
2024-06-03 14:45:34 -0300

    fist commit

---
+++ src/main/java/com/alura/challengeliteralura/ChallengeLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challengeliteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiterAluraApplication.class, args);
+	}
+
+}
